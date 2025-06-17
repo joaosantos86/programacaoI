4bc663f3ab40be25700c5a599d3dba0d682b4af3
joaosantos86 <joaops41224@gmail.com>
2025-06-17 15:28:31 -0300

    feat: add solution for exercise 04

--- Operador_Ternario/ListaExercicios/04_vogal.js
+++ Operador_Ternario/ListaExercicios/04_vogal.js
@@ -1,1 +1,22 @@
 // Construa um programa que peça para o usuário informar um caractere e apresente a mensagem descrevendo se este caractere é uma vogal.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Informe UM caractere: ", function(caractere) {
+  const mensagem = 
+    (caractere === 'a' || caractere === 'A' ||
+     caractere === 'e' || caractere === 'E' ||
+     caractere === 'i' || caractere === 'I' ||
+     caractere === 'o' || caractere === 'O' ||
+     caractere === 'u' || caractere === 'U')
+    ? "O caractere é uma vogal."
+    : "O caractere NÃO é uma vogal.";
+
+  console.log(mensagem);
+  rl.close();
+});
