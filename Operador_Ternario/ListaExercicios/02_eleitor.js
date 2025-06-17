c820969c19c23dc06b761971535af4c6754b75fe
joaosantos86 <joaops41224@gmail.com>
2025-06-17 15:28:23 -0300

    feat: add solution for exercise 02

--- Operador_Ternario/ListaExercicios/02_eleitor.js
+++ Operador_Ternario/ListaExercicios/02_eleitor.js
@@ -1,1 +1,14 @@
 // Considerando que no Brasil é possível votar a partir dos 16 anos de idade, faça um script que peça o ano de nascimento de uma pessoa e informe se ela poderá votar no ano atual. Ignore mês e dia de nascimento.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Informe seu ano de nascimento", function(nascimento){
+    nascimento = Number(nascimento)
+    nascimento <= 2009 ? console.log("Você ja pode voltar") : console.log("Você não pode votar")
+    rl.close()
+})
